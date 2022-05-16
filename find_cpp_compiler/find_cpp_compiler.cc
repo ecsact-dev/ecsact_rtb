06ddb1244755ade09a7a45ae48ef7ad33ef7b44d
Kelwan <austin@kelway.ca>
2022-05-16 13:07:26 -0400

    Check which compiler user is using, currently supports clang

--- find_cpp_compiler/find_cpp_compiler.cc
+++ find_cpp_compiler/find_cpp_compiler.cc
@@ -6,5 +6,36 @@ result::find_cpp_compiler ecsact::rtb::find_cpp_compiler
 	( const options::find_cpp_compiler& options
 	)
 {
+	auto clang = boost::process::search_path("clang");
+
+	if(clang == "") {
+		std::cout << "No clang found" << std::endl;
+	} else {
+		std::cout << "Getting clang version" << std::endl;
+		auto compiler_out = get_compiler_version(clang);
+		for(auto& line : compiler_out) {
+			std::cout << line << std::endl;
+		}
+	}
 	return {};
+}
+
+std::vector<std::string> ecsact::rtb::get_compiler_version
+	( boost::filesystem::path compiler_path
+	)
+{
+	namespace bp = boost::process;
+	bp::ipstream is;
+	bp::child c(compiler_path, "--version", bp::std_out > is);
+
+	std::vector<std::string> data;
+	std::string line;
+
+    while (c.running() && std::getline(is, line) && !line.empty())
+        data.push_back(line);
+
+    c.wait();
+
+    return data;
+
 }
--- find_cpp_compiler/find_cpp_compiler.hh
+++ find_cpp_compiler/find_cpp_compiler.hh
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <optional>
+#include <iostream>
+
+#include "boost/process/search_path.hpp"
+#include "boost/process.hpp"
+
 namespace ecsact::rtb {
 	namespace options {
 		struct find_cpp_compiler {
@@ -9,11 +15,16 @@ namespace ecsact::rtb {
 
 	namespace result {
 		struct find_cpp_compiler {
-			
+			std::optional<boost::filesystem::path> path;
+			std::optional<std::string> compiler_version;
 		};
 	}
 
 	result::find_cpp_compiler find_cpp_compiler
 		( const options::find_cpp_compiler& options
+		);
+	
+	std::vector<std::string> get_compiler_version
+		( boost::filesystem::path compiler_path
 		);
 }
