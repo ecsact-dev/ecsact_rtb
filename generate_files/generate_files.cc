d2e30a5f26516c9966fb0aba7c211ec8e0a248a3
Ezekiel Warren <zekewarren@gmail.com>
2022-09-29 16:05:44 -0700

    stdout/stderr for codegen subcommands

--- generate_files/generate_files.cc
+++ generate_files/generate_files.cc
@@ -5,9 +5,89 @@
 #include <fstream>
 #include <boost/process.hpp>
 
+#include "util/report_subcommand_output.hh"
+
 namespace fs = std::filesystem;
 namespace bp = boost::process;
 using namespace ecsact::rtb;
+
+static void run_codegen
+	( const options::generate_files&  options
+	, fs::path                        output_directory
+	, std::vector<std::string>        plugins
+	)
+{
+	using namespace std::string_literals;
+
+	std::vector<std::string> codegen_proc_args;
+	codegen_proc_args.reserve(
+		plugins.size() + 2 + options.ecsact_file_paths.size()
+	);
+	codegen_proc_args.emplace_back("codegen"s);
+	for(auto& plugin : plugins) {
+		codegen_proc_args.emplace_back("--plugin="s + plugin);
+	}
+	codegen_proc_args.emplace_back("--outdir="s + output_directory.string());
+
+	for(auto& p : options.ecsact_file_paths) {
+		codegen_proc_args.push_back(p.string());
+	}
+
+	ecsact::rtb::util::report_subcommand_output codegen_stdout;
+	ecsact::rtb::util::report_subcommand_output codegen_stderr;
+	bp::child codegen_proc(
+		bp::exe(options.ecsact_cli_path.string()),
+		bp::args(codegen_proc_args),
+		bp::std_out > codegen_stdout.output_stream,
+		bp::std_err > codegen_stderr.output_stream
+	);
+
+	auto subcommand_id = static_cast<ecsact_rtb::subcommand_id_t>(
+		codegen_proc.id()
+	);
+	options.reporter.report(ecsact_rtb::subcommand_start_message{
+		.id = subcommand_id,
+		.executable = options.ecsact_cli_path.string(),
+		.arguments = codegen_proc_args,
+	});
+
+	codegen_stdout.start<ecsact_rtb::subcommand_stdout_message>(
+		options.reporter,
+		subcommand_id
+	);
+	codegen_stderr.start<ecsact_rtb::subcommand_stderr_message>(
+		options.reporter,
+		subcommand_id
+	);
+
+	codegen_stdout.wait();
+	codegen_stderr.wait();
+	codegen_proc.wait();
+
+	auto exit_code = codegen_proc.exit_code();
+
+	options.reporter.report(ecsact_rtb::subcommand_end_message{
+		.id = subcommand_id,
+		.exit_code = exit_code,
+	});
+
+	if(exit_code != 0) {
+		std::string codegen_command = options.ecsact_cli_path.string() + " ";
+		for(auto arg : codegen_proc_args) {
+			codegen_command += arg + " ";
+		}
+		options.reporter.report(ecsact_rtb::info_message{
+			.content = "Codegen Command: "s + codegen_command
+		});
+
+		options.reporter.report(ecsact_rtb::error_message{
+			.content =
+				"Ecsact codegen failed wth exit code " + std::to_string(exit_code),
+		});
+		std::cout.flush();
+		std::exit(exit_code);
+	}
+}
 
 result::generate_files ecsact::rtb::generate_files
 	( const options::generate_files& options
@@ -28,68 +108,15 @@ result::generate_files ecsact::rtb::generate_files
 	fs::create_directory(include_dir);
 	fs::create_directory(src_dir);
 
-	std::vector<fs::path> header_paths;
-	std::vector<fs::path> source_file_paths;
-
-	std::vector<std::string> codegen_proc_args{
-		"codegen",
-		"--plugin=cpp_header"s,
-		"--plugin=cpp_meta_header"s,
-		"--plugin=cpp_systems_header"s,
-		"--plugin=systems_header"s,
-		"--outdir="s + include_dir.string(),
-	};
-	codegen_proc_args.reserve(
-		codegen_proc_args.size() + options.ecsact_file_paths.size()
-	);
-
-	for(auto& p : options.ecsact_file_paths) {
-		codegen_proc_args.push_back(p.string());
-	}
-
-	bp::child codegen_proc(
-		bp::exe(options.ecsact_cli_path.string()),
-		bp::args(codegen_proc_args),
-		bp::std_out > bp::null,
-		bp::std_err > bp::null
-	);
-
-	auto subcommand_id = static_cast<ecsact_rtb::subcommand_id_t>(
-		codegen_proc.id()
-	);
-	options.reporter.report(ecsact_rtb::subcommand_start_message{
-		.id = subcommand_id,
-		.executable = options.ecsact_cli_path.string(),
-		.arguments = codegen_proc_args,
+	run_codegen(options, include_dir, {
+		"cpp_header"s,
+		"cpp_meta_header"s,
+		"cpp_systems_header"s,
+		"systems_header"s,
 	});
-
-	codegen_proc.wait();
-
-	auto exit_code = codegen_proc.exit_code();
-
-	options.reporter.report(ecsact_rtb::subcommand_end_message{
-		.id = subcommand_id,
-		.exit_code = exit_code,
-	});
-
-	if(exit_code != 0) {
-		std::string codegen_command = options.ecsact_cli_path.string();
-		for(auto arg : codegen_proc_args) {
-			codegen_command += arg + " ";
-		}
-		options.reporter.report(ecsact_rtb::info_message{
-			.content = "Codegen Command: "s + codegen_command
-		});
-
-		options.reporter.report(ecsact_rtb::error_message{
-			.content =
-				"Ecsact codegen failed wth exit code " + std::to_string(exit_code),
-		});
-		std::exit(exit_code);
-	}
 
 	return {
 		.include_dir = include_dir,
-		.source_file_paths = source_file_paths,
+		.source_file_paths = {},
 	};
 }
